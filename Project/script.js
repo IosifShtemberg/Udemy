6c38e5dc64757734f9d284ead3ddec33b20b8e20
Iosif <ex0tic2793@gmail.com>
2020-05-01 22:33:22 +0300

    Задание 4

--- Project/script.js
+++ Project/script.js
@@ -1,25 +1,97 @@
 'use strict';
 
-let money , time;
+let money, time;
 
 function start() {
-    money = +prompt("Ваш бюджет на месяц?");
-    time = prompt("Введите дату в формате чч-мм-гггг");
+  money = +prompt('Ваш бюджет на месяц?');
+  time = prompt('Введите дату в формате чч-мм-гггг');
 
-    while(isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц?");
-    }
+  while (isNaN(money) || money == '' || money == null) {
+    money = +prompt('Ваш бюджет на месяц?');
+  }
 }
 start();
 
 let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true   
- };
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: true,
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt('Введите обязательную статью расходов в этом месяце');
+      let b = prompt('Во сколько обойдется?');
+
+      if (
+        typeof a === 'string' &&
+        typeof a != null &&
+        typeof b != null &&
+        a != '' &&
+        b != '' &&
+        a.length < 50
+      ) {
+        console.log('done');
+        appData.expenses[a] = b;
+      } else {
+        console.log('Error');
+        i--;
+      }
+    }
+  },
+  detectDayBudget: function () {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert('ежедневный бюджет: ' + appData.moneyPerDay);
+  },
+  detectLevel: function () {
+    if (appData.moneyPerDay < 100) {
+      console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+      console.log('Высокий уровень достатка');
+    } else {
+      console.log('Произошла ошибка');
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt('Какова сумма накоплений'),
+        percent = +prompt('Под какой процент?');
+
+      appData.mounthIncome = (save / 100 / 12) * percent;
+      alert('Доход в месяц с вашего депозита: ' + appData.mounthIncome);
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let q = 1; q < 4; q++) {
+      let a = prompt('Статья необязательных расходов?');
+      appData.optionalExpenses[q] = a;
+    }
+  },
+  chooseIncome: function () {
+    let items = prompt(
+      'Что принесёт дополнительный доход? (перечислите через запятую)'
+    );
+    if (typeof items === 'string' && typeof items != null && items != '') {
+      appData.income = items.split(',');
+      appData.income.push(prompt('Может что-то ещё?'));
+      appData.income.sort();
+    } else {
+      console.log('Неверные данные');
+    }
+    appData.income.forEach(function (item, i) {
+      alert('Способы доп зароботка: ' + (i + 1) + '.' + item);
+    });
+  },
+};
+
+for (let key in appData) {
+  console.log(
+    'Наша программа включает в себя данные: ' + key + ': ' + appData[key]
+  );
+}
 
 /////////////////////////////////////////////////////////////////////////////
 // let i = 0;
@@ -27,14 +99,14 @@ let appData = {
 //     let a = prompt("Введите обязательную статью расходов в этом месяце");
 //     let b = prompt("Во сколько обойдется?");
 //     i++;
-//  if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null 
+//  if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null
 // && a != '' && b != '' && a.length < 50 ) {
 //     console.log("done");
 //     appData.expenses[a] = b;
 // } else {
 //     console.log("Error");
 //     i--;
-// } 
+// }
 // i++;
 // }
 
@@ -43,9 +115,9 @@ let appData = {
 // let i = 0;
 // do {
 //     let a = prompt("Введите обязательную статью расходов в этом месяце");
-//     let b = prompt("Во сколько обойдется?"); 
+//     let b = prompt("Во сколько обойдется?");
 
-//  if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null 
+//  if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null
 // && a != '' && b != '' && a.length < 50 ) {
 //     console.log("done");
 //     appData.expenses[a] = b;
@@ -59,65 +131,3 @@ let appData = {
 // while(i<2);
 
 ////////////////////////////////////////////////////////////////////////////
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++){
-        let a = prompt("Введите обязательную статью расходов в этом месяце");
-        let b = prompt("Во сколько обойдется?");
-    
-        if ( (typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null 
-        && a != '' && b != '' && a.length < 50 ) {
-            console.log("done");
-            appData.expenses[a] = b;
-        } else {
-            console.log("Error");
-            i--;
-        } 
-    }
-}
-chooseExpenses();
-
-function detectDayBudget(){
-    appData. moneyPerDay = (appData.budget / 30).toFixed();
-    alert("ежедневный бюджет: " + appData.moneyPerDay);
-}
-detectDayBudget();
-
-function detectLevel() {
-
-    if (appData.moneyPerDay < 100) {
-        console.log("Минимальный уровень достатка");
-
-}    else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
-        console.log("Средний уровень достатка");
-
-}    else if (appData.moneyPerDay > 2000){
-        console.log("Высокий уровень достатка");
-
-}    else {
-        console.log("Произошла ошибка");
-}
-}
-detectLevel();
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("Какова сумма накоплений"),
-            percent = +prompt("Под какой процент?");
-
-        appData.mounthIncome = save/100/12*percent;
-        alert("Доход в месяц с вашего депозита: " + appData.mounthIncome);
-    }
-}
-checkSavings();
-
-function chooseOptExpenses() {                             
-
-    for (let q = 1; q < 4; q++) {   
-
-        let a = prompt("Статья необязательных расходов?");
-        appData.optionalExpenses[q] = a;
-    }
-
-}
-chooseOptExpenses();
